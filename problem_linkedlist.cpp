e72bde5da369144ac9b98b85ecbef2bd88310c63
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2020-02-07 00:32:19 +0100

    LinkedList class braought into function except the partition method

--- problem_linkedlist.cpp
+++ problem_linkedlist.cpp
@@ -1,10 +1,10 @@
  /*
- Given a linked list of numbers and a pivot k, 
+ Given a linked list of numbers and a threshold x, 
  partition the linked list so that 
- all nodes less than k come before nodes greater than or equal to k.
+ all nodes less than x come before nodes greater than or equal to x.
 
 For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 
-and k = 3, 
+and x = 3, 
 the solution could be 1 -> 0 -> 5 -> 8 -> 3.208
 */
 # include <iostream>
@@ -13,9 +13,10 @@ class Node
 {
     public:
         float m_value;
-        Node* next;
+        Node* m_next;
         
-        explicit Node(float value = 0) : m_value{value}
+        explicit Node(float value = 0, Node* nextNode = nullptr) : 
+        m_value{value}, m_next(nextNode)
         {}
     
 };
@@ -25,24 +26,54 @@ class LinkedList
      public:
         Node* m_firstNode;
         Node* m_lastNode;
+
+        // Constructor definition
         LinkedList (Node* firstNode = nullptr) : m_firstNode{firstNode}
         {}
+
         // method to add node to the beginning
         void addToBeginning(int value)
         {
             Node* beginningNode = new Node(value);
             if (m_firstNode)
             {
-                beginningNode -> next = m_firstNode;
+                beginningNode -> m_next = m_firstNode;
                 m_firstNode = beginningNode;
+            } 
+            else
+            {
+                m_firstNode = beginningNode;
+                m_lastNode = beginningNode;
             }
             
         }
+
         // method to add Node to the end of the linkedlist
         void addToEnd(int value)
         {
-
+            if(m_lastNode)
+            {
+                m_lastNode -> m_next = new Node(value);
+            }
         }     
+
+        // traverses the LinkedList and puts the elements smaller than threshold
+        // to the first one and the bigger ones to the second one
+        void partition(int thresholdVal, LinkedList &smallerList, LinkedList &greaterList)
+        {
+
+        }
+
+        void traverseAndPrint(void)
+        {
+            Node* currNode = m_firstNode;
+            while (currNode)
+            {
+                std::cout << currNode -> m_value << " -> ";
+                currNode = currNode -> m_next;
+            }
+            std::cout << "NULL" << std::endl;
+        }
  };
 
  int main()
@@ -50,6 +81,10 @@ class LinkedList
      LinkedList tLinkList;
 
      tLinkList.addToBeginning(5);
+     tLinkList.addToBeginning(3);
+     tLinkList.addToEnd(8);
+
+     tLinkList.traverseAndPrint();
      std::cout << "No errors" << std::endl;
      return 0;
  }
