f3039b5006ea69125a66d2f552961a3e39f148c6
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2020-02-06 22:59:22 +0100

    addToBeginning method of LinkedList class is implemented

--- problem_linkedlist.cpp
+++ problem_linkedlist.cpp
@@ -7,7 +7,7 @@ For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3
 and k = 3, 
 the solution could be 1 -> 0 -> 5 -> 8 -> 3.208
 */
-
+# include <iostream>
 //Node class represent a node in a LinkedList class, which holds a data and points to the next node
 class Node
 {
@@ -15,7 +15,7 @@ class Node
         float m_value;
         Node* next;
         
-        explicit Node(float value) : m_value{value}
+        explicit Node(float value = 0) : m_value{value}
         {}
     
 };
@@ -24,7 +24,32 @@ class LinkedList
  {
      public:
         Node* m_firstNode;
-        LinkedList (Node firstNode) : m_firstNode{&firstNode}
+        Node* m_lastNode;
+        LinkedList (Node* firstNode = nullptr) : m_firstNode{firstNode}
         {}
-     
+        // method to add node to the beginning
+        void addToBeginning(int value)
+        {
+            Node* beginningNode = new Node(value);
+            if (m_firstNode)
+            {
+                beginningNode -> next = m_firstNode;
+                m_firstNode = beginningNode;
+            }
+            
+        }
+        // method to add Node to the end of the linkedlist
+        void addToEnd(int value)
+        {
+
+        }     
  };
+
+ int main()
+ {
+     LinkedList tLinkList;
+
+     tLinkList.addToBeginning(5);
+     std::cout << "No errors" << std::endl;
+     return 0;
+ }
